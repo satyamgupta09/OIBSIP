806c3ff2df30f4ae67ed511cf7268a47c41c481b
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2023-08-12 00:21:01 +0530

    Add files via upload

--- A BASIC TO-DO WEBAPP/src/App.js
+++ A BASIC TO-DO WEBAPP/src/App.js
@@ -1,1 +1,42 @@
+import "./styles.css";
+import { Component } from "react";
+import { List } from "./List";
+import { Form } from "./Form";
 
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { text: "Do the laundry" },
+        { text: "Iron the clothes" },
+        { text: "Go for a walk" }
+      ]
+    };
+  }
+
+  handleAdd=(text_to_add)=>{
+    const new_todos=[...this.state.todos,text_to_add];
+    this.setState({
+      todos:new_todos
+    })
+  }
+
+  handleRemove=(text)=>{
+    const val=this.state.todos.filter((todo)=>(todo.text!==text));
+    this.setState({
+      todos:val
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <span>Todo-Do-App</span>
+        <Form  handleAdd={this.handleAdd} />
+        <List todos={this.state.todos} handleRemove={this.handleRemove}/>
+      </div>
+    );
+  }
+}
